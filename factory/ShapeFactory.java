9ecebd6e808300ad98a6993524a9e09be7b6ad9d
Ethan Kelly <72452934+ethankelly@users.noreply.github.com>
2020-12-22 14:08:57 +0000

    Create ShapeFactory.java

---
+++ factory/ShapeFactory.java
@@ -0,0 +1,22 @@
+package io.github.ethankelly;
+
+public class ShapeFactory {
+
+    // Get an object of type shape
+    public Shape getShape(String shapeType) {
+        if(shapeType == null){
+            return null;
+        }
+        if(shapeType.equalsIgnoreCase("CIRCLE")){
+            return new Circle();
+
+        } else if(shapeType.equalsIgnoreCase("SQUARE")){
+            return new Square();
+
+        } else if(shapeType.equalsIgnoreCase("RECTANGLE")){
+            return new Rectangle();
+
+        }
+        return null;
+    }
+}
