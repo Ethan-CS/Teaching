41073010b03024d8ca1d22adebeefbf3f60cd676
Ethan Kelly <72452934+ethankelly@users.noreply.github.com>
2021-01-14 16:07:50 +0000

    Create HelloWorld.java

---
+++ ProgIt-resources/HelloWorld.java
@@ -0,0 +1,18 @@
+package examples;
+
+/**
+ * First exercise for week 1:
+ * Write a program that prints "Hello World!" to the console
+ * 
+ * @author Ethan
+ *
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
